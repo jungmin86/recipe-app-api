9acf9ac87fd535819b86a572494fffdba1a640d6
김정민 <kimjungmin@gimjeongmin-ui-MacBookPro.local>
2024-02-10 16:25:51 +0900

    Simple Unit Test

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,6 +2,7 @@
 Calculator functions
 """
 
+
 def add(x, y):
-    """ Adding x and y """
+    """ Add x and y and return result"""
     return x + y
--- app/app/tests.py
+++ app/app/tests.py
@@ -3,14 +3,15 @@ Sample tests
 """
 
 from django.test import SimpleTestCase
+
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
-    """Test the calc module.
-    """
+    """Test the calc module."""
+
     def test_add_numbers(self):
-        """Test adding numbers together
-        """
+        """Test adding numbers together"""
         res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
