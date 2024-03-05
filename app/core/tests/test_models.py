e21f6ce62c0df3c34753730dc89fbb25cb693acd
jungmin86 <jmkimm0p@gmail.com>
2024-03-05 12:40:13 +0900

    Modify test code

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -52,7 +52,7 @@ class ModelTests(TestCase):
 
     def test_create_recipe(self):
         """Test creating a recipe is successful."""
-        user = get_user_model.objects.create_user(
+        user = get_user_model().objects.create_user(
             email='test@example.com',
             password='testpass123',
         )
