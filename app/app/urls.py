af2d072dc37deba96ab1f9352667b643bbb97f9c
jungmin86 <jmkimm0p@gmail.com>
2024-03-07 15:57:26 +0900

    Test and Implement recipe listing API

--- app/app/urls.py
+++ app/app/urls.py
@@ -21,12 +21,13 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
+    path("admin/", admin.site.urls),
+    path("api/schema/", SpectacularAPIView.as_view(), name="api-schema"),
     path(
-        'api/docs/',
-        SpectacularSwaggerView.as_view(url_name='api-schema'),
-        name='api-docs'
+        "api/docs/",
+        SpectacularSwaggerView.as_view(url_name="api-schema"),
+        name="api-docs",
     ),
-    path('api/user/', include('user.urls'))
+    path("api/user/", include("user.urls")),
+    path("api/recipe/", include("recipe.urls")),
 ]
