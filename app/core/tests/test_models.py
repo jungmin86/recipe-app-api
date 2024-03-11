d6338f7cdb4a27782721cf2114a9331e8f41beaf
jungmin86 <jmkimm0p@gmail.com>
2024-03-11 18:05:18 +0900

    Modify format

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -48,7 +48,10 @@ class ModelTests(TestCase):
 
     def test_create_superuser(self):
         """Test creating a superuser."""
-        user = get_user_model().objects.create_superuser("test@example.com", "test123")
+        user = get_user_model().objects.create_superuser(
+            "test@example.com",
+            "test123",
+        )
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
