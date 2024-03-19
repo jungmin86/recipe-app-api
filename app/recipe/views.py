0e189ebc51f3a385e780fc837faadb2e954ae2c2
jungmin86 <jmkimm0p@gmail.com>
2024-03-20 01:14:18 +0900

    Add OpenApi to view

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,7 +1,12 @@
 """
 Views for the recipe APIs.
 """
-
+from drf_spectacular.utils import (
+    extend_schema_view,
+    extend_schema,
+    OpenApiParameter,
+    OpenApiTypes,
+)
 from rest_framework import (
     viewsets,
     mixins,
