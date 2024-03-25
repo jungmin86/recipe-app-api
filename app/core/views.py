f2788fd7efd220e53d2ab96716e849f255b15e7f
jungmin86 <jmkimm0p@gmail.com>
2024-03-26 01:36:50 +0900

    Added health check api.

---
+++ app/core/views.py
@@ -0,0 +1,10 @@
+"""
+Core views for app.
+"""
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(["GET"])
+def health_check(request):
+    return Response({"healthy": True})
